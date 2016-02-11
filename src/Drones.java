0c67e4c62c5580ee1228be3ee8d608be756437d2
Daniele Reda <redad.93@gmail.com>
2016-02-11 19:53:21 +0100

    drones starting

---
+++ src/Drones.java
@@ -0,0 +1,26 @@
+
+public class Drones {
+	
+	private int maxWeight;
+	private int[] products;
+
+	public void setMaxWeight(int maxWeight) {
+		maxWeight = maxWeight;
+	}
+	
+	public void initializeProducts(int nrProducts) {
+		products = new int[nrProducts];
+		for(int i = 0;i<nrProducts;i++){
+			products[i]=0;
+		}
+	}
+	
+	public void load(int productToLoad) {
+	}
+	
+	
+	
+	
+	
+	
+}
